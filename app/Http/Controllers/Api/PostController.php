b6672dab392d711d79cc23a22dfd6bcfcf8176fb
sazid49 <sazidfahim49@gmail.com>
2022-11-16 13:24:42 +0600

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function index($id=null)
+    {
+           $posts = $id?Post::find($id):Post::all();
+        //    dd($posts->toArray());
+        //    return $posts;
+
+        return $posts;
+    }
+
+  
+
+    public function addpost(Request $request)
+    {
+         
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return ['message'=>'input plz'];
+        }
+       
+        $post = new Post;
+        $post->title=$request->title;
+        $post->description=$request->description;
+        $result =$post->save();
+
+        if($result)
+        {
+            return ['message'=>'data save success'];
+        }else{
+            return ['message'=>'data faild'];
+        }
+    }
+
+    public function updatePost(Request $request,$id)
+    {
+        $post = Post::find($id);
+        $post->title = $request->title;
+        $post->description=$request->description;
+        $result=$post->update();
+        if($result)
+        {
+            return ['message'=>'update success'];
+        }else{
+            return ['message'=>'update faild'];
+
+        }
+    }
+
+    public function deletepost($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+        return ['message'=>'data delete success'];
+    }
+
+    public function searchpost($name)
+    {
+        $search = Post::query()
+                        ->where('title','LIKE',"%$name%")
+                        ->orWhere('description','LIKE',"%$name%")
+                        ->get();
+                        if(count($search)){
+                            return $search;
+                        } else {
+                            return [['Result', 'No records found']];
+                        }
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Api\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('getPost/{id?}',[PostController::class,'index'])->name('api.posts');
+
+Route::post('addPost',[PostController::class,'addpost']);
+Route::put('updatePost/{id}',[PostController::class,'updatepost']);
+Route::delete('deletePost/{id}',[PostController::class,'deletepost']);
+Route::get('searchPost/{name}',[PostController::class,'searchpost']);
